bcd9696de9c2c75e1bcecaf96207c5b5a17ec280
AppreciativeGeek <mnpencildoodle@gmail.com>
2021-07-30 12:47:52 -0500

    Initial Commit

---
+++ Za Hunter/Za_HunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Za_HunterApp.swift
+//  Za Hunter
+//
+//  Created by James on 7/30/21.
+//
+
+import SwiftUI
+
+@main
+struct Za_HunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
